190e524c30c258ba7fab38fea89ff76342789dcf
hoRaaa <1941861784@qq.com>
2020-01-10 10:54:34 +0800

    Add files via upload

---
+++ allData.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class allData
+{
+    //滑板数值
+    public float maxSpeed = 30;//最高速度
+    public float acceleration = 2;//加速度
+    public float offsetSpeed = 5;//偏移速度
+    public float slowDown = 1;//减速度
+
+    //游戏数值
+    public int hp = 3;//生命值
+    public int time = 10;//时间
+    public int addTime = 15;//加时间
+
+    //场景数值
+    public float generatePoint = -14000;//生成点
+    public float startPoint = -4000;//起点
+    public float endPoint = 6000;//终点，删除点
+
+
+}
---
+++ controlBoard.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 滑板的控制，前进，偏移
+/// </summary>
+public class controlBoard : MonoBehaviour
+{
+    public float speed = 0;//实时速度
+    public float maxSpeed;//最高速度
+    public float acceleration;//加速度
+    public float offsetSpeed;//偏移速度
+    public float slowDown;//减速
+    public void setData()//获取数值
+    {
+        maxSpeed = GetComponent<allData>().maxSpeed;
+        acceleration = GetComponent<allData>().acceleration;
+        offsetSpeed = GetComponent<allData>().offsetSpeed;
+    }
+
+    public void forward()//滑板速度
+    {
+        if (speed < maxSpeed)
+        {
+            speed += acceleration;
+        }
+        this.transform.Translate(0, 0, speed * Time.deltaTime);
+    }
+
+    public void offset()//滑板偏移，转向
+    {
+        if()
+    }
+
+    void Start()
+    {
+        setData();
+    }
+    void Update()
+    {
+        forward();
+        offset();
+    }
+}
