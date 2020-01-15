237af51714e09e676c3feb9f05ac3c2b86c9f291
hoRaaa <1941861784@qq.com>
2020-01-15 19:16:29 +0800

    Add files via upload

---
+++ updataItems.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class updataItems : MonoBehaviour
+{
+    public float[] X;//生成位置
+    public GameObject energy;//能量体
+    public GameObject whereEnergy = GameObject.Find("energy");//能量体父物体
+    public GameObject obstacles;//障碍物
+    public GameObject whereObstacles = GameObject.Find("obstacles");//障碍物父物体
+    public allData data;//所有数值
+
+    public int random0 = 6;//障碍物随机生成点
+    public int randomE;//能量体随机生成点
+    public int wayRandom;//随机办法
+    public float timer = 0;//计时器
+    public int cycleIndex;//循环次数
+
+    void Start()
+    {
+        setData();
+        InvokeRepeating("doUpdata", 1, 1);
+    }
+
+    public void setData()//获取数值
+    {
+        data = GameObject.Find("allData").GetComponent<allData>();
+        X = data.X;
+    }
+
+    public void generateEnergy(int whereRandom)//生成能量体
+    {
+        GameObject newEnergy = Instantiate(energy, new Vector3(X[whereRandom], 15, -500), energy.transform.rotation);
+        newEnergy.transform.parent = whereEnergy.transform;
+        Destroy(newEnergy, 15);
+    }
+    public void generateObstacles(int whereRandom)//生成障碍物
+    {
+        GameObject newObstacles = Instantiate(obstacles, new Vector3(X[whereRandom], 15, -500), obstacles.transform.rotation);
+        newObstacles.transform.parent = whereObstacles.transform;
+        Destroy(newObstacles, 15);
+    }
+
+    public void generate00()//生成办法1
+    {
+        randomE = Random.Range(1, 5);
+        generateEnergy(randomE);
+        while (randomE == random0)
+        {
+            random0 = Random.Range(1, 5);
+        }
+        generateObstacles(random0);
+    }
+
+    public void generate01()//生成办法2
+    {
+
+    }
+
+    public void generate02()//生成办法3
+    {
+
+    }
+
+    public void doUpdata()//刷新物件
+    {
+        if (GameObject.Find("board").GetComponent<controlBoard>().speed > 250)
+        {
+            generate00();
+        }
+    }
+}
---
+++ updataRoad.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class updataRoad : MonoBehaviour
+{
+    public allData data;//所有数值
+    public float generatePoint;//生成点
+    public float startPoint;//起点
+    public float endPoint;//终点，删除点
+    public GameObject whereRoad = GameObject.Find("roadScene");//道路父物体
+    public GameObject road;
+
+    public void setData()//获取数值
+    {
+        data = GameObject.Find("allData").GetComponent<allData>();
+        generatePoint = data.generatePoint;
+        startPoint = data.startPoint;
+        endPoint = data.endPoint;
+    }
+
+    /*public void Road01()//生成道路01
+    {
+        if (this.transform.position.z == startPoint)
+        {
+            GameObject newRoad = Instantiate(road01, new Vector3(0, 0, generatePoint), road.transform.rotation);
+            newRoad.transform.parent = whereRoad.transform;
+
+        }
+    }*/
+
+    public void generateRoad()//生成道路
+    {
+        if (this.transform.position.z == startPoint)
+        {
+            /*int i = Random.Range(1, 3);//随机选择生成的道路
+            switch (i)
+            {
+                case '1':Road01();break;
+            }*/
+
+            GameObject newRoad = Instantiate(road, new Vector3(0, 0, generatePoint), road.transform.rotation);
+            newRoad.transform.parent = whereRoad.transform;
+
+        }
+        if (this.transform.position.z > endPoint)
+        {
+            this.transform.position = new Vector3(0, 0, startPoint);
+        }
+    }
+
+    void Start()
+    {
+        setData();
+    }
+
+    void Update()
+    {
+        generateRoad();
+    }
+}
