be387175c8774670ccd7092626b9acce01cd78e4
hoRaaa <1941861784@qq.com>
2020-03-26 14:00:59 +0800

    Add files via upload
    上传分数

--- uploadScore.php
+++ uploadScore.php
@@ -2,6 +2,17 @@
 include('connection.php');
 
 $sql = "SELECT * FROM `users_data` ORDER BY `users_data`.`user_score` DESC";
+$sql1 = "SELECT * FROM users_data WHERE user_name = '".$_POST['userName']."'";
+
+$result1 = mysqli_query($connect,$sql);
+$row1 = mysqli_fetch_assoc($result);
+
+if($_POST['userScore'] > $row1['user_score'])
+{
+	$sql2 = "UPDATE `users_data` SET `user_score` = '".$_POST['userScore']."' WHERE `users_data`.`user_name` = '".$_POST['userName']."';";
+	$result2 = mysqli_query($connect,$sql);
+}
+
 $result = mysqli_query($connect,$sql);
 
 for($x=0;$x<5;$x++){
